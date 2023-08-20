73c0e004414b2cb625b81d92ae6d2c740a78abe0
dfabianus <fabianmueller100295@gmail.com>
2023-08-20 14:47:16 +0200

    commit

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 
 const REST_URL = "http://128.131.132.179:8080/lpims/rest/v1/"
 const CALLURL_REACTORS = REST_URL + "reactors/"
+const CALLURL_REACTORS_RUNNING = REST_URL + "reactors?running=true"
+const CALLURL_PROC_RUNNING = REST_URL + "processes?running=true"
+const CALLURL_PROC_NAME = REST_URL + "processes?name="
+const CALLURL_PORT_NAME = REST_URL + "ports?name="
+const CALLURL_SIGNALS_FROM_PROC_ID = REST_URL + "signals?processId="
 
 var USERNAME string = "student"
 var PASSWORD string = "bioVT"
@@ -20,13 +25,20 @@ type Reactor struct {
 		Name    string  `json:"name"`
 		Volume  float32 `json:"volume"`
 		Running bool    `json:"running"`
+		// Process Process `json:"process"`
+	} `json:"data"`
+}
+type Process struct {
+	Data []struct {
+		Id   uint   `json:"id"`
+		Name string `json:"name"`
 	} `json:"data"`
 }
 
 func main() {
 	fmt.Println("Hello, go")
 	fmt.Println(REST_URL)
-	get_reactors()
+	get_process("Fermenter 22 2023 Week 10 006")
 }
 
 func get_reactors() {
@@ -40,6 +52,48 @@ func get_reactors() {
 	for _, element := range reactors.Data {
 		fmt.Println("ID:", element.Id, "Name:", element.Name, "Volume:", element.Volume, "Running:", element.Running)
 	}
+}
+
+func get_running_reactors() {
+	var reactors Reactor
+	body := get_request(CALLURL_REACTORS_RUNNING)
+	err := json.Unmarshal(body, &reactors)
+	if err != nil {
+		fmt.Println("Error unmarshaling json:", err)
+		return
+	}
+	for _, element := range reactors.Data {
+		fmt.Println("ID:", element.Id, "Name:", element.Name, "Volume:", element.Volume, "Running:", element.Running)
+	}
+	//fmt.Println("Response Body:", string(body))
+}
+
+func get_running_processes() {
+	var processes Process
+	body := get_request(CALLURL_PROC_RUNNING)
+	err := json.Unmarshal(body, &processes)
+	if err != nil {
+		fmt.Println("Error unmarshaling json:", err)
+		return
+	}
+	for _, element := range processes.Data {
+		fmt.Println("ID:", element.Id, "Name:", element.Name)
+	}
+	//fmt.Println("Response Body:", string(body))
+}
+
+func get_process(process_name string) {
+	var processes Process
+	body := get_request(CALLURL_PROC_NAME + process_name)
+	err := json.Unmarshal(body, &processes)
+	if err != nil {
+		fmt.Println("Error unmarshaling json:", err)
+		return
+	}
+	for _, element := range processes.Data {
+		fmt.Println("ID:", element.Id, "Name:", element.Name)
+	}
+	//fmt.Println("Response Body:", string(body))
 }
 
 func get_request(callurl string) []byte {
