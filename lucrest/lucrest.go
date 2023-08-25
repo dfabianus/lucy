7443efab5e2dfec3636b02db7d51f6c86b39988a
dfabianus <fabianmueller100295@gmail.com>
2023-08-25 18:34:09 +0200

    lucrest added line

--- lucrest/lucrest.go
+++ lucrest/lucrest.go
@@ -141,6 +141,7 @@ func GetProcessById(process_id string) { // does not work yet
 func GetSignalsByProcessId(process_id string) { // does not work yet
 	var signals Signal
 	body := get_request(CALLURL_SIGNALS_FROM_PROC_ID + process_id)
+	fmt.Println(CALLURL_SIGNALS_FROM_PROC_ID + process_id)
 	//body := get_request("http://128.131.132.179:8080/lpims/rest/v1/signals?processId=8983")
 	//body := get_request(CALLURL_SIGNALS_FROM_PROC_ID + "12212")
 	err := json.Unmarshal(body, &signals)
